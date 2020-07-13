0c65f35916f0556462335d4a32f399a4119510cc
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-13 14:36:33 +0200

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -4,9 +4,10 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(10,GPIO.IN)
 i = 5
 for x in range(5):
-	reps.rep("i", i)
+	reps.rep("gpio.in", GPIO.input())
 	GPIO.output(8,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(8,GPIO.LOW)
