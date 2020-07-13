aa2f0c8a07155811dc2cd2bfae52d7f0e0e4dd5c
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-13 14:58:25 +0200

    Add files via upload

--- reports.py
+++ reports.py
@@ -1,6 +1,6 @@
 import time as tts
 def rep(name, val):
-	f= open("report.txt","w+")
+	f= open("report.txt","a")
 	f.write("value \t" + name +" equals\t%d \t at time %d \n"%(val, tts.time()))
 	f.close()
 
