5bbc77b216393e4a26e1a3bcfe95366736e4d5b7
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-13 10:28:03 +0200

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
-import reports
+import reports as reps
 from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
 for x in range(5):
-	rep("ahoj")
+	reps.rep("ahoj")
 	GPIO.output(8,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(8,GPIO.LOW)
