1aa1f32fe2cc92cca5e8bcb9e87343572433d5e8
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-13 12:24:33 +0200

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT, initial=GPIO.LOW)
 i = 5
 for x in range(5):
-	reps.rep("ahoj", i)
+	reps.rep("i", i)
 	GPIO.output(8,GPIO.HIGH)
 	sleep(1)
 	GPIO.output(8,GPIO.LOW)
