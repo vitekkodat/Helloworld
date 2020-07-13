1d40dbcd4c404e8d49c8170081ebef225c46f9ff
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-13 12:29:59 +0200

    Add files via upload

--- reports.py
+++ reports.py
@@ -1,7 +1,5 @@
 def rep(name, val):
 	f= open("report.txt","w+")
-	f.write("value \t" + name +" equals\t")
-	f.write(val)
-	f.write("\n")
+	f.write("value \t" + name +" equals\t%d\n"%val)
 	f.close()
 
