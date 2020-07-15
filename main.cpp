bad0109aa549f1aa65afd2d89f0ba7b2e6de1c6f
vitekkodat <68062226+vitekkodat@users.noreply.github.com>
2020-07-15 13:49:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <tchar.h>
+
 #include <urlmon.h>
 
 #pragma comment(lib, "urlmon.lib")
 int main()
 {
-	HRESULT hr = URLDownloadToFile ( NULL, _T("http://www.cplusplus.com/forum/beginner/109232/"), _T("/var/lib/jenkins/workspace/Hello_C++/report.txt"), 0, NULL );
+	HRESULT hr = URLDownloadToFile ( NULL, "http://www.cplusplus.com/forum/beginner/109232/", "/var/lib/jenkins/workspace/Hello_C++/report.txt", 0, NULL );
 	return 0;
 }
